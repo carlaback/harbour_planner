9e0e644184f76b4256b80f1711829f548d3e1e64
Carl Åback <calle.aback@gmail.com>
2025-05-15 09:42:30 +0200

    class boat

---
+++ app/strategy_engine.py
@@ -0,0 +1,10 @@
+from typing import List, Dict
+from datetime import date
+
+
+class Boat:
+    def __init__(self, id: int, width: float, arrival: date, departure: date):
+        self.id = id
+        self.width = width
+        self.arrival = arrival
+        self.departure = departure
